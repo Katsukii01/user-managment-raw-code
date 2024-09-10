1f5f544c7b739e243863031a90fc5be4d7eaef72
Shad0w <97676458+Katsukii01@users.noreply.github.com>
2024-09-10 10:19:41 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import UserTable from './components/UserTable';
 
 const App: React.FC = () => {
   return (
-    <section className='relative w-full h-screen mx-auto flex items-center  flex-col bg-gradient'>
+    <section className='relative w-full h-screen mx-auto flex items-center  flex-col bg-gradient overflow-y-scroll'>
       <h1 className='text-5xl text-white text-bold py-8'>User Management</h1>
       <UserTable />
     </section>
